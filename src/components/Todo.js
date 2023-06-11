e383b2b590ceaca00a12658eaa22620dcdb58cb9
unknown <patrick.linguerri@hotmail.com>
2023-06-11 13:05:18 +0200

    host name on todo.js

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -6,7 +6,7 @@ function Todo() {
   const [message, setMessage] = useState("");
   const [todoList, setTodoList] = useState([]);
 
-  const url = "http://ec2-3-87-186-10.compute-1.amazonaws.com:3001/todoList";
+  const url = "http://ip-172-31-84-181.ec2.internal:3001/todoList";
 
   const inputText = (e) => {
     setMessage(e.target.value);
